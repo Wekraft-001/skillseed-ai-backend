8c0b7acc67cf2dd79dacf9625a948c168834b0ae
YannickZahinda <ymulikuza@gmail.com>
2025-05-13 14:53:32 +0200

    add sanitizer to avoid malicious input

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Body, Post } from "@nestjs/common";
+import { Controller, Body, Post, UsePipes } from "@nestjs/common";
 import { CreateUserDto } from "./dtos";
 import { AuthService } from "./auth.service";
+import { SanitizePipe } from "../sanitizer/sanitize.pipe";
 
 @Controller('auth')
 export class AuthController {
     constructor (private readonly authService: AuthService) {}
 
     @Post('register')
+    @UsePipes(new SanitizePipe())
     register(@Body() dto: CreateUserDto) {
         return this.authService.registerUser(dto);
     }
 
     @Post('signin')
+    @UsePipes(new SanitizePipe())
     sigin(@Body() body: { firstName: string; password: string }) {
         return this.authService.signin(body.firstName, body.password);
     }

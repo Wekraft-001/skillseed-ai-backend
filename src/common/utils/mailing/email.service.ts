a247dcaf1576f2aba65867c38b44f788402aae76
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2025-07-05 10:26:23 +0200

    corrected email service configuration to ensure that users get emails

--- src/common/utils/mailing/email.service.ts
+++ src/common/utils/mailing/email.service.ts
@@ -3,18 +3,29 @@ import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class EmailService {
-  private transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: Number(process.env.MAIL_PORT),
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-  });
+  private transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: Number(process.env.MAIL_PORT),
+      auth: {
+        user: process.env.MAIL_USER || 'api',
+        pass: process.env.MAIL_PASS || '7f608d469e261486705539e674a462dd',
+        authMethod: 'PLAIN,LOGIN',
+      },
+      tls: {
+        rejectUnauthorized: process.env.MAIL_STARTTLS === 'true',
+      },
+      debug: true,
+    });
+    console.log('MAIL_USER:', process.env.MAIL_USER);
+    console.log('MAIL_PASS:', process.env.MAIL_PASS);
+  }
 
   async sendSchoolOnboardingEmail(email: string, password: string) {
     const mailOptions = {
-      from: '"Skillseed" <skillseed.wekraft.co>',
+      from: process.env.MAIL_FROM,
       to: email,
       subject: 'Your Skillseed School Account',
       html: `
@@ -30,6 +41,11 @@ export class EmailService {
       `,
     };
 
-    await this.transporter.sendMail(mailOptions);
+    try {
+      const info = await this.transporter.sendMail(mailOptions);
+      console.log('✅ Email sent:', info.response);
+    } catch (error) {
+      console.error('❌ Failed to send email:', error);
+    }
   }
 }
